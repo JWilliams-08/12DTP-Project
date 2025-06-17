a08fa4ead28df89edca88b89f911767ae82165bc
JWilliams-08 <Jessicanicolewilliams2008@gmail.com>
2025-06-17 14:25:25 +1200

    added players into teams and deleted player data that is in an incorrect format

--- routes.py
+++ routes.py
@@ -5,18 +5,24 @@ import sqlite3
 #create the flask app
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('fill-ins.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route('/')
 def home():
     # Connect to the SQLite database
-    conn = sqlite3.connect('fill-ins.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT DISTINCT date FROM Draw')
-    dates = cursor.fetchall()
-    # Close the connection
+    conn = get_db_connection()
+    dates = conn.execute('SELECT DISTINCT date FROM Draw').fetchall()
+    teams = conn.execute('SELECT name FROM Team').fetchall()
     conn.close()
-    return render_template('home.html', title='HOME', dates=dates)
+    return render_template('home.html', title='HOME', dates=dates, teams=teams)
 
 
+@app.route('/results')
+def results():
+
+    return render_template('results.html', title='RESULTS')
 if __name__ == '__main__':
     app.run(debug=True)
