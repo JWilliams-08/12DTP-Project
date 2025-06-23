3cf4b248162c69a824fc6f9dedc023eb02089682
JWilliams-08 <Jessicanicolewilliams2008@gmail.com>
2025-06-24 10:08:23 +1200

    form request submits and saves in database

--- routes.py
+++ routes.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect 
 import sqlite3
 
 
 #create the flask app
 app = Flask(__name__)
 
+# Function to get a database connection
 def get_db_connection():
     conn = sqlite3.connect('fill-ins.db')
     conn.row_factory = sqlite3.Row
     return conn
+
 
 @app.route('/')
 def home():
@@ -19,8 +21,10 @@ def home():
     conn.close()
     return render_template('home.html', title='HOME', dates=dates, teams=teams)
 
+
 @app.route('/get_players/<team_name>')
 def get_players(team_name):
+    # get players from a selected team
     conn = get_db_connection()
     players = conn.execute('''
         SELECT player_name
@@ -29,6 +33,23 @@ def get_players(team_name):
     ''', (team_name,)).fetchall()
     conn.close()
     return jsonify({'players': [p[0] for p in players]})
+
+
+@app.route('/submit_request', methods=['POST'])
+def submit_request():
+    date = request.form['date']
+    team = request.form['team']
+    player = request.form['player']
+    conn = get_db_connection()
+    conn.execute('''
+                 INSERT INTO FillInRequest (date, team_name, player_name)
+                 VALUES (?, ?, ?)
+''' , (date, team, player))
+    conn.commit()
+    conn.close()
+
+    #redirect user to results page
+    return redirect('/results')
 
 @app.route('/results')
 def results():
