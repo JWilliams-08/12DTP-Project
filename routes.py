e36d13eb9d96403d227eec4c5bf737341b608ba3
JWilliams-08 <Jessicanicolewilliams2008@gmail.com>
2025-06-26 12:34:52 +1200

    function can succesfully find valid teams that have spare players or at a differnt time/day

--- routes.py
+++ routes.py
@@ -1,20 +1,84 @@
-from flask import Flask, render_template, jsonify, request, redirect 
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 import sqlite3
 
 
-#create the flask app
+#Create the flask app
 app = Flask(__name__)
 
-# Function to get a database connection
+#Functions
+
 def get_db_connection():
+    #Connect to the SQLite database
     conn = sqlite3.connect('fill-ins.db')
     conn.row_factory = sqlite3.Row
     return conn
 
 
+def get_valid_fillins(team, date, player):
+    #get all valid fill-ins
+    grade_order = [10,12,14,18]
+    conn = get_db_connection()
+
+    #get information about the team
+    team = conn.execute('SELECT * FROM Team WHERE name = ?', (team,)).fetchone()
+    if not team:
+        conn.close()
+        return []
+    grade = int(team['grade'])
+    div = int(team['division'])
+    day = team['day']
+    time = team['time']
+    print(grade, div, day, time)
+
+    #workout the next youngest age grade
+    try:
+        target_index = grade_order.index(grade)
+    except ValueError:
+        conn.close()
+        return []
+    next_younger_grade = grade_order[target_index - 1] if target_index > 0 else None
+
+    print(next_younger_grade)
+    #Query eleible teams:
+    if next_younger_grade: #if there is a younger age group, query both that and lower divisions of same grade
+        eligible_teams = conn.execute('''
+            SELECT * FROM Team
+            WHERE (grade = ? AND division < ?)
+            OR (grade = ?)                     
+        ''',(str(grade), (div), str(next_younger_grade))).fetchall() 
+
+    else: #if no younger age groups, query lower divisions of same grade
+        eligible_teams = conn.execute('''
+            SELECT * FROM Team
+            WHERE grade = ? AND division < ?
+        ''', (str(grade), str(div))).fetchall()
+
+    #check if there are any eligible teams and if not return empty list
+    if not eligible_teams:
+        conn.close()
+        return []
+    
+    #filter out any teams playing at the same time unless players > 4
+    valid_team_names = []
+    for team in eligible_teams:
+        if team['day'] != day or team['time'] != time:
+            valid_team_names.append(team['name'])
+        else:
+            player_count = conn.execute('SELECT COUNT(*) FROM PlayerTeam WHERE team_name = ?', (team['name'],)).fetchone()[0]
+            if player_count > 4:
+                valid_team_names.append(team['name'])
+    
+    #if not teams are valid, return empty list
+    if not valid_team_names:
+        conn.close()
+        return []
+    print(valid_team_names)
+
+
+
 @app.route('/')
 def home():
-    # Connect to the SQLite database
+    #Connect to the SQLite database
     conn = get_db_connection()
     dates = conn.execute('SELECT DISTINCT date FROM Draw').fetchall()
     teams = conn.execute('SELECT name FROM Team').fetchall()
@@ -24,7 +88,7 @@ def home():
 
 @app.route('/get_players/<team_name>')
 def get_players(team_name):
-    # get players from a selected team
+    #get players from a selected team
     conn = get_db_connection()
     players = conn.execute('''
         SELECT player_name
@@ -35,10 +99,8 @@ def get_players(team_name):
     return jsonify({'players': [p[0] for p in players]})
 
 
-@app.route('/submit_request', methods=['POST'])
-
-def submit_request():
-    #submit a fill-in request
+@app.route('/results', methods=['POST'])
+def results():
     date = request.form['date']
     team = request.form['team']
     player = request.form['player']
@@ -50,14 +112,10 @@ def submit_request():
     conn.commit()
     conn.close()
 
-    #redirect user to results page
-    return redirect('/results', team=team, date=date, player=player)
-
-@app.route('/results')
-def results():
-    conn = get_db_connection()
+    #call function to get valid fill-ins
+    valid_players = get_valid_fillins(team, date, player)
     # get fill-in requests 
 
-    return render_template('results.html', title='RESULTS')
+    return render_template('results.html', date=date, team=team, valid_players=valid_players)
 if __name__ == '__main__':
     app.run(debug=True)
