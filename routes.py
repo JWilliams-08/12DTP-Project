a0676e3656f217fd4582e939f4eed3b0ce37f1b8
JWilliams-08 <Jessicanicolewilliams2008@gmail.com>
2025-09-07 17:16:34 +1200

    added code to host and test across multiple devices

--- routes.py
+++ routes.py
@@ -152,5 +152,14 @@ def not_found_error(error):
 def internal_error(error):
     return render_template('500.html'), 500
 
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
+# For mobile testing - allows connections from any device on your network
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+       debug=True,
+       host='0.0.0.0',
+       port=5000,
+       threaded=True
+    )
